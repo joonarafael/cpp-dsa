e95ea8fb4dd75cb4a7d50adf366c99dfde5fe0ce
joonarafael <joona.kettunen21@gmail.com>
2024-04-16 14:25:40 +0300

    room alloc

--- part1/roomallocation.cpp
+++ part1/roomallocation.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 struct Booking {
     long long arrival;
     long long departure;
-    int roomNumber;
+    int index;
 };
 
-bool sortByDepartureTime(const Booking &a, const Booking &b) {
-    return a.departure < b.departure;
+bool sortByArrivalTime(const Booking &a, const Booking &b) {
+    return a.arrival < b.arrival;
 }
 
 int main() {
@@ -23,10 +21,45 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         cin >> bookings[i].arrival >> bookings[i].departure;
-        bookings[i].roomNumber = -1;
+        bookings[i].index = i;
     }
 
-    sort(bookings.begin(), bookings.end(), sortByDepartureTime);
+    sort(bookings.begin(), bookings.end(), sortByArrivalTime);
+
+    vector<int> roomAllocations(n);
+    set<pair<int, int>> emptyRooms;
+
+        int roomNumber = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (emptyRooms.empty()) {
+            roomNumber += 1;
+
+            roomAllocations[bookings[i].index] = roomNumber;
+            emptyRooms.insert({bookings[i].departure, roomNumber});
+        } else {
+            auto it = emptyRooms.begin();
+            pair<int, int> room = *it;
+
+            if (room.first < bookings[i].arrival) {
+                roomAllocations[bookings[i].index] = room.second;
+
+                emptyRooms.erase(*emptyRooms.begin());
+                emptyRooms.insert({bookings[i].departure, room.second});
+            } else {
+                roomNumber += 1;
+
+                roomAllocations[bookings[i].index] = roomNumber;
+                emptyRooms.insert({bookings[i].departure, roomNumber});
+            }
+        }
+    }
+
+    cout << roomNumber << endl;
+
+    for (int i = 0; i < n; ++i) {
+        cout << roomAllocations[i] << " ";
+    }
 
     cout << endl;
 
